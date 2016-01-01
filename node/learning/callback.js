c0f2f9a245e76efdd4d2e0760c8975f935ee7af1
ederign <ignatowicz@gmail.com>
2016-01-01 20:26:35 -0200

    .

--- node/learning/callback.js
+++ node/learning/callback.js
@@ -1,32 +1,31 @@
-var maxTime=1000;
+var maxTime = 1000;
 
-var evenDoubler = function (v, callback){
-    var waiTime = Math.floor(Math.random()*(maxTime+1));
+var evenDoubler = function(v, callback) {
+    var waiTime = Math.floor(Math.random() * (maxTime + 1));
 
-    if(v%2){
-        setTimeout(function(){
+    if (v % 2) {
+        setTimeout(function() {
             callback(new Error("Odd input"));
         }, waiTime)
-    }
-    else{
-        setTimeout(function(){
-            callback(null, v*2, waiTime)
+    } else {
+        setTimeout(function() {
+            callback(null, v * 2, waiTime)
         }, waiTime)
     }
 
 };
 
 
-var handleResults = function(err, results, time){
-	if(err){
+var handleResults = function(err, results, time) {
+    if (err) {
         console.log("ERROR:" + err.message)
-    }else{
-        console.log("The results are " + results + " ("+time+" ms)");
+    } else {
+        console.log("The results are " + results + " (" + time + " ms)");
     }
 };
 
 for (var i = 1; i < 11; i++) {
-   evenDoubler(i, handleResults);
+    evenDoubler(i, handleResults);
 };
 
 console.log("===================================");
--- node/learning/mathfun.js
+++ node/learning/mathfun.js
@@ -1,20 +1,19 @@
-var maxTime=1000;
+var maxTime = 1000;
 
-var evenDoubler = function (v, callback){
-    var waiTime = Math.floor(Math.random()*(maxTime+1));
+var evenDoubler = function(v, callback) {
+    var waiTime = Math.floor(Math.random() * (maxTime + 1));
 
-    if(v%2){
-        setTimeout(function(){
+    if (v % 2) {
+        setTimeout(function() {
             callback(new Error("Odd input"));
         }, waiTime)
-    }
-    else{
-        setTimeout(function(){
-            callback(null, v*2, waiTime)
+    } else {
+        setTimeout(function() {
+            callback(null, v * 2, waiTime)
         }, waiTime)
     }
 
 };
 
 module.exports.evenDoubler = evenDoubler;
-module.exports.foo = "bar"; 
+module.exports.foo = "bar";
