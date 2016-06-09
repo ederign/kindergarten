57f3cab6311d0094d2f70a80c04d73c4172c5641
ederign <ignatowicz@gmail.com>
2016-06-09 07:02:17 -0300

    chapter 1 examples

---
+++ cs/sicp/book1_1.scm
@@ -0,0 +1,34 @@
+(define  (abs x)
+	(cond (< x 0)(- x))
+		  (else x)))
+
+(abs -1)
+
+(define (absIF x)
+	(if (< x 0)
+		(- x) 
+		x)))
+
+(absIF -1)
+
+(define (testAnd x)
+	(and (> x 5) (< x 10)))
+
+(testAnd 6)
+
+
+(define (>= x y)
+	(or (> x y)(= x y)))
+
+(>= 6 8)
+
+
+(>= 8 6)
+
+(define (>=2 x y)
+	(not (< x y)))
+
+(>=2 6 8)
+
+
+(>=2 8 6)
