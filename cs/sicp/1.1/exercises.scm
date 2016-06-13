4e76d6753506dc8626bb54cd4de1ec8bdcf6b299
ederign <ignatowicz@gmail.com>
2016-06-13 07:11:14 -0300

    more exercises from 1.1

--- cs/sicp/1.1/exercises.scm
+++ cs/sicp/1.1/exercises.scm
@@ -55,9 +55,45 @@
 (sum_sqr_2_largers 8 -3 -27)
 ; 73
 
+;;;1.4
+(define (a-plus-abs-b a b)
+	((if (> b 0) + -) a b))
+
+(a-plus-abs-b 10 4)
 
 
+(a-plus-abs-b 10 -4)
 
+;;;1.5
+(define (p) (p))
+(define (test x y)
+	(if (= x 0)
+		0
+		y))
 
+;;;(test 0 (p))
+ #|
+ scheme default is applicative-order evaluation
+all the arguments to Scheme procedures are evaluated when the procedure is applied
+Using applicative-order evaluation, the evaluation of (test 0 (p)) never terminates, because (p) is infinitely expanded to itself:
+
+ (test 0 (p)) 
+  
+ (test 0 (p)) 
+  
+ (test 0 (p)) 
+
+ Using normal-order evaluation , the expression evaluates, step by step, to 0:
+
+ (test 0 (p)) 
+  
+ (if (= 0 0) 0 (p)) 
+  
+ (if #t 0 (p)) 
+  
+ 0 
+
+In contrast, normal-order languages delay evaluation of procedure arguments until the actual argument values are needed. Delaying evaluation of procedure arguments until the last possible moment (e.g., until they are required by a primitive operation) is called lazy evaluation.
+|#
 
 
